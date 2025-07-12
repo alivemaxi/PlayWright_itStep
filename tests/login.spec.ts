7a24ca84becc4f38030ab3f1ac559e1df6dfc403
alivemaxi <maks-bereziuk@seznam.cz>
2025-07-12 15:58:00 +0200

    Первый коммит: инициализация проекта и добавление

---
+++ tests/login.spec.ts
@@ -0,0 +1,7 @@
+/*import { test } from '@playwright/test'; 
+import { LoginPage } from '../pages/LoginPage'; 
+test('Тест логина', async ({ page }) => { 
+const loginPage = new LoginPage(page); 
+await page.goto('https://example.com/login'); 
+await loginPage.login('myUser', 'myPassword'); 
+}); */
