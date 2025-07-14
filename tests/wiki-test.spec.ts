11b068bcadf5a4cef6a31dca95ebe5e62714feae
alivemaxi <maks-bereziuk@seznam.cz>
2025-07-14 21:04:07 +0200

    Изменение в ветке A

--- tests/wiki-test.spec.ts
+++ tests/wiki-test.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 test('Open Wikipedia and check title', async ({ page }) => {
   await page.goto('https://www.wikipedia.org');
   await expect(page).toHaveTitle(/Wikipedia/);
+  console.log("This is first commit in new branch")
 });
